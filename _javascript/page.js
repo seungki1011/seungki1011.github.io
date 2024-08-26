8dc0a971b96d42e5731c619acf9a0a6f7ce5c74c
seungki1011 <kimseungki1011@gmail.com>
2024-08-26 23:28:53 +0900

    Fix cardClick

--- _javascript/page.js
+++ _javascript/page.js
@@ -7,11 +7,11 @@ initSidebar();
 initTopbar();
 initClipboard();
 basic();
-cardClick();
+// cardClick();
 
-function cardClick(event, url) {
-    // Prevent navigation if an <a> tag or a descendant is clicked
-        if (event.target.tagName.toLowerCase() !== 'a' && !event.target.closest('a')) {
-            window.location.href = url;
-        }
-    }
+// function cardClick(event, url) {
+//     // Prevent navigation if an <a> tag or a descendant is clicked
+//         if (event.target.tagName.toLowerCase() !== 'a' && !event.target.closest('a')) {
+//             window.location.href = url;
+//         }
+//     }
